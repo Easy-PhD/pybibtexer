c5a44544153eb5e38789cad622a333645b36c894
NextAI <nextartifintell@users.noreply.com>
2025-10-24 10:04:49 +0800

    feat(examples/): add examples/

---
+++ examples/local_config.py
@@ -0,0 +1,6 @@
+import os
+
+local_options = {
+    "full_json_c": os.path.expanduser(os.path.join("", "conferences.json")),
+    "full_json_j": os.path.expanduser(os.path.join("", "journals.json"))
+}
